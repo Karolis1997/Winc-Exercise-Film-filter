de85ce04594d788d7dc76062ae256ed14fb294ab
Karolis1997 <110409354+Karolis1997@users.noreply.github.com>
2022-10-17 12:22:40 +0200

    Add files via upload

---
+++ Film finder/code.js
@@ -0,0 +1,48 @@
+function showall(){
+    document.getElementById("XavengersX")   .style.display = 'block';
+    document.getElementById("XprincessX")   .style.display = 'block';
+    document.getElementById("XbatmanX")     .style.display = 'block';
+    document.getElementById("XmenX")        .style.display = 'block';
+    document.getElementById("XnewmoviesX")  .style.display = 'none ';
+};
+
+function showXmen(){
+    document.getElementById("XavengersX")   .style.display = 'none' ;
+    document.getElementById("XprincessX")   .style.display = 'none' ;
+    document.getElementById("XbatmanX")     .style.display = 'none' ;
+    document.getElementById("XmenX")        .style.display = 'block';
+    document.getElementById("XnewmoviesX")  .style.display = 'none' ;
+};
+
+
+function showAvenger(){
+    document.getElementById("XmenX")        .style.display = 'none' ;
+    document.getElementById("XprincessX")   .style.display = 'none' ;
+    document.getElementById("XbatmanX")     .style.display = 'none' ;
+    document.getElementById("XavengersX")   .style.display = 'block';
+    document.getElementById("XnewmoviesX")  .style.display = 'none' ;
+};
+
+function showPrincess(){
+    document.getElementById("XavengersX")   .style.display = 'none' ;
+    document.getElementById("XmenX")        .style.display = 'none' ;
+    document.getElementById("XbatmanX")     .style.display = 'none' ;
+    document.getElementById("XprincessX")   .style.display = 'block';
+    document.getElementById("XnewmoviesX")  .style.display = 'none' ;
+};
+
+function showBatman(){
+    document.getElementById("XavengersX")   .style.display = 'none' ;
+    document.getElementById("XprincessX")   .style.display = 'none' ;
+    document.getElementById("XmenX")        .style.display = 'none' ;
+    document.getElementById("XbatmanX")     .style.display = 'block';
+    document.getElementById("XnewmoviesX")  .style.display = 'none' ;
+};
+
+function showNew(){
+    document.getElementById("XavengersX")   .style.display = 'none' ;
+    document.getElementById("XprincessX")   .style.display = 'none' ;
+    document.getElementById("XmenX")        .style.display = 'none' ;
+    document.getElementById("XbatmanX")     .style.display = 'none' ;
+    document.getElementById("XnewmoviesX")  .style.display = 'block';
+};
